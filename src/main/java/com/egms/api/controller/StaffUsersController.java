6c39e5aafa33720d2e2480e718f52c6c6601a882
Thesura <thesuracom@gmail.com>
2020-03-02 13:51:03 +0530

    heroku

--- src/main/java/com/egms/api/controller/StaffUsersController.java
+++ src/main/java/com/egms/api/controller/StaffUsersController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "/api/staffusers")
 public class StaffUsersController {
 
