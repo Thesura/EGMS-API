b4c420dfde356ffd675874a3ac582a4aa2b9366d
Thesura <thesuracom@gmail.com>
2020-03-16 16:51:42 +0530

    heroku

--- src/main/java/com/egms/api/service/IReportsRepository.java
+++ src/main/java/com/egms/api/service/IReportsRepository.java
@@ -10,9 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface IReportsRepository extends JpaRepository<Report, Integer> {
 
     @Query("SELECT c1 FROM Coordinate c1\n" +
-            "JOIN Coordinate c2\n" +
-            "ON c1.area_id = c2.area_id\n" +
-            "WHERE (?1>c1.lng1 AND ?2<c1.lat1)" +
+           "WHERE (?1>c1.lng1 AND ?2<c1.lat1)" +
             " AND (?1<c2.lng3 AND ?2>c2.lat3)")
     Coordinate getAreaId(double longitude1, double latitude1);
 
