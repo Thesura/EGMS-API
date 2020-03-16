a32f0ae9bd148a6a681c6027f9c291ad719c6e35
Thesura <thesuracom@gmail.com>
2020-03-16 16:55:39 +0530

    heroku

--- src/main/java/com/egms/api/service/IReportsRepository.java
+++ src/main/java/com/egms/api/service/IReportsRepository.java
@@ -11,7 +11,7 @@ public interface IReportsRepository extends JpaRepository<Report, Integer> {
 
     @Query("SELECT c1 FROM Coordinate c1\n" +
            "WHERE (?1>c1.lng1 AND ?2<c1.lat1)" +
-            " AND (?1<c2.lng3 AND ?2>c2.lat3)")
+            " AND (?1<c1.lng3 AND ?2>c1.lat3)")
     Coordinate getAreaId(double longitude1, double latitude1);
 
 }
