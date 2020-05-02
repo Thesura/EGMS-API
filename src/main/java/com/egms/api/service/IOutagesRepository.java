c671999755138212376eed19ba85080146e2faed
Thesura <thesuracom@gmail.com>
2020-05-02 20:07:16 +0530

    heroku

--- src/main/java/com/egms/api/service/IOutagesRepository.java
+++ src/main/java/com/egms/api/service/IOutagesRepository.java
@@ -1,12 +1,16 @@
 package com.egms.api.service;
 
 import com.egms.api.model.Outage;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface IOutagesRepository extends CrudRepository<Outage, Integer> {
+    @Transactional
+    @Modifying
     @Query("DELETE FROM Outage AS O WHERE O.status = ?1")
     void deleteFixedOutages(String status);
 }
